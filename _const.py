cf2168378bf33fc38db9f4f76b2b6f2872daa18f
mroldann <martinroldan@outlook.com>
2021-08-19 17:05:12 +0200

    Commit pre query enhancement

--- _const.py
+++ _const.py
@@ -9,6 +9,18 @@ class PATHS:
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = PATHS.KEY.as_posix()
 
+class BQ_guardians:
+    dataset = 'subgraphs'
+    table = 'guardians'
+
+
+class URLS:
+    class SUBGRAPH:
+        class COURT:
+            MAINNET = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-court-v2-mainnet'
+
+
+
 COLS_TO_DROP_EVENTS = [
 "inputs",
 "payable",
@@ -21,12 +33,6 @@ COLS_TO_DROP_EVENTS = [
 
 
 
-
+THE_GRAPH
 ADDRESS_COURT = "0xab647b8fd9e370448d4eeb96582fe839f3d0bb24"
 FUNC_SIGN_GUARDIANS_ACTIVATED = "0x3bb1ff40e32fd6b2e3b81f6a7441852a5c72ec25d116b141b1a1a2e8f0404095"
-
-
-class URLS:
-    class SUBGRAPH:
-        class COURT:
-            MAINNET = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-court-v2-mainnet'
